25d41a32c1fae1aba4467dd76d35148aaec09597
unearthlabs <luke@unearthlabs.com>
2014-11-08 16:08:02 +0000

    Code Re-Write

--- arduiset.ino
+++ arduiset.ino
@@ -1,18 +1,46 @@
+// Cicero44bc 15:24 08 Nov 2014 - www.breakfastlabs.com
+//
+// This sketch allows a safe reset of the Arduino using code and one wire from a digital pin to the reset pin.
+// I found when using a continuous servo I was getting very slight drift. I discovered that if the servo is not
+// attached in code, therefore not receiving any instructions it would not drift or flutter at all.
+//
+// This sketch checks if this is the first boot, runs and action if so (openClose() in this case - to accommodate
+// a requirement for my aquarium), sets a value in EEPROM (so it is persistent after reset) resets the Arduino,
+// checks EEPROM if its first boot, does nothing (in this case) when this is confirmed as the 'second boot' and
+// resets the value in EEPROM for next time.
+//
+// Notes
+//
+// When programing your Arduino be sure to disconnect the wire to RST if you want your Arduino to auto-reset
+// after the upload completes.
+//
+// Outputs to serial at 115200 for debugging.
+//
+// Credits
+//
+// Reset method - http://weblog.jos.ph/development/arduino-reset-hack/
+//
+// Shout out - Thanks to ALL at ultimateReef.net for your help.
+
 #include <Servo.h>
 #include <EEPROM.h>
  
-Servo myservo;
+Servo servo;
 int servoPin = 9;
-int resetPin = 7;
-int bootCheckPointer = 1;
+int resetPin = 7; // Digital Pin 7, connected to RST
+int bootCheckPointer = 1; // Checks position one.
 bool secondBoot = false;
            
 void setup()
 {
-  digitalWrite(resetPin, HIGH); //We need to set it HIGH immediately on boot
-  pinMode(resetPin,OUTPUT);     //We can declare it an output ONLY AFTER it's HIGH
-  if (!checkSecondBoot()) {
-  myservo.attach(servoPin);
+  digitalWrite(resetPin, HIGH); // Pin must be set to HIGH immediately on boot.
+  pinMode(resetPin,OUTPUT);     // Declare it as an output AFTER it's HIGH
+  if (checkSecondBoot()) {  // Check EEPROM to confirm if this is this is the first boot - Checking a Variable first is better due to save the EEPROM (Limited R/W's).
+    secondBoot = true;
+    resetEEPROM();
+  }
+  if (!secondBoot) {     // Only attach the servo if it is the first boot. On a second boot servo will not be attached.
+    servo.attach(servoPin);
   }
   Serial.begin(115200);
 
@@ -21,63 +49,45 @@ void setup()
  
 void loop()
 {
-  //Serial.println("resetPin:" && resetPin && ":servoPin:" && servoPin + ".");
-  //  Serial.println(resetPin);
-  //  Serial.println(servoPin);
-//  Serial.println(EEPROM.read(bootCheckPointer));
   Serial.println("loop start");
-  if (!secondBoot) {
-//    Serial.println("If !secondBoot");
-//    Serial.println(checkSecondBoot());
-    if (!checkSecondBoot()) { // If this is the first boot
-    Serial.println("FirstBoot");
-  //    Serial.println(checkSecondBoot());
+  if (!secondBoot) { 
+    Serial.println("FirstBoot. Now set EEPROM value, open and Close then reset...");
       setSecondBoot(); // Set EEPROM pointer
       openClose();
       resetArduino();
-    }
-    else
-    {
-      resetEEPROM();
-      secondBoot = true;
-      Serial.println("secondBoot = True");
-    //  Serial.println(checkSecondBoot());
-    }
   }
-    
+  else
+  {
+    Serial.println("Second boot");
+  } 
 }
 
-boolean checkSecondBoot() {
+boolean checkSecondBoot() { // http://arduino.cc/en/Reference/EEPROMRead
 	if (EEPROM.read(bootCheckPointer) != 255)
 	{
 		return true; // This is the second boot.
-Serial.println("checkSecondBoot = true");
 	}
 	else
 	{
 		return false; // This is the first boot.
-Serial.println("checkSecondBoot = false");
 	}
 }
 
 void openClose() {
-  myservo.write(0);
+  servo.write(0);
+  Serial.println("Open");
   delay(1000);
-  myservo.write(88); 
-  Serial.println("  myservo.write(180);");
+  servo.write(86);
+  Serial.println("Stay Open for 60secs");
   delay(60000);
-  myservo.write(180);
+  servo.write(180);
+  Serial.println("Close");
   delay(1000);
-  myservo.write(88);
-  Serial.println("  myservo.write(0);");
-  delay(1500);
-  myservo.write(88);
-    Serial.println("  myservo.write(87);");
-  delay(1000);
-    Serial.println("openClose END");
+  servo.write(86);
+  Serial.println("Stay Closed");
 }
 
-void resetEEPROM() {
+void resetEEPROM() { // http://arduino.cc/en/Reference/EEPROMWrite
   EEPROM.write(bootCheckPointer, 255); // (ADDRESS, VALUE)
   Serial.println("EEPROM values reset.");
 }
